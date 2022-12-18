c64e6f7cbe281eb86a1feb6ac34d05323a39ae1d
rajnish <rajnishprasadkalwar@gmail.com>
2022-12-18 16:31:45 +0000

    First Java Program

---
+++ firstprog.java
@@ -0,0 +1,15 @@
+package main.webapp;
+import java.util.*;
+
+public class Main {
+
+public static void main(String[] args) {
+		     Scanner sc=new Scanner(System.in);
+		     System.out.println("ënter values");
+				int a=sc.nextInt();
+				int b=sc.nextInt();
+				int sum=a+b;
+				System.out.print(sum);
+				}
+
+}
